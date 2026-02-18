84c932f62b8984eb3943073232879102c96192f3
Ryan Kuang <96274506+RyanKuang2004@users.noreply.github.com>
2026-02-19 10:09:47 +1100

    feat: add package exports via __init__.py

---
+++ aujobsscraper/scrapers/__init__.py
@@ -0,0 +1,6 @@
+from aujobsscraper.scrapers.base_scraper import BaseScraper
+from aujobsscraper.scrapers.seek_scraper import SeekScraper
+from aujobsscraper.scrapers.gradconnection_scraper import GradConnectionScraper
+from aujobsscraper.scrapers.prosple_scraper import ProspleScraper
+
+__all__ = ["BaseScraper", "SeekScraper", "GradConnectionScraper", "ProspleScraper"]
